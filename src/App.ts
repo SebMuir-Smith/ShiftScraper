b165a1df6c36bcca95cc163d3af7897489ba2b9a
Sebastian Muir-Smith <sebastian@muirsmith.com>
2019-07-01 16:54:12 +1000

    Add working website object importing

--- src/App.ts
+++ src/App.ts
@@ -2,14 +2,17 @@
 
 import { Website } from "./Website";
 
+// For some reason ./ is not working here
+let obga:Website = require(process.cwd() + "/data/obgaHeader.json");
+
+console.log(obga);
+
 let myWebsite: Website = new Website("https://threepointsix.azurewebsites.net/api/post", "Sample Employer",
     [{ key: "Message", value: "nice" }, { key: "Authors", value: ["BigMemeDaddy"] }], [{ key: "Authors", value: ["BigMemeDaddy"] }]);
 
 myWebsite.GetData()
-    .then((response) => myWebsite.ScrapeData(response));
+    //.then((response) => myWebsite.ScrapeData(response));
 
 // Second call for debugging
 myWebsite.GetData()
-    .then((response) => console.log(response));
-
-    
+    //.then((response) => console.log(response));

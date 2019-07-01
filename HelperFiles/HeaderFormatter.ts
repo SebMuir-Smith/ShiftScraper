d50be1f5cc8002497bd67f1e0294e0c4af9548ce
Sebastian Muir-Smith <sebastian@muirsmith.com>
2019-07-01 16:24:35 +1000

    Fix helper file

--- HelperFiles/HeaderFormatter.ts
+++ HelperFiles/HeaderFormatter.ts
@@ -1,39 +1,50 @@
-const readline = require('readline');
+// Little helper file that converts the output of chrome's 
+// network tool into a valid json
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+var fs = require('fs');
 
-rl.question('Enter string: ', (answer:string) => {
-  console.log(CleanString(answer));
-  rl.close();
-});
+// Read in data
+try {  
+    var data = fs.readFileSync('file.txt', 'utf8');
+    console.log(CleanString(data.toString()));    
+} catch(e) {
+    console.log('Error:', e.stack);
+}
 
 function CleanString(input:string):string{
 for (let i = 0; i < input.length; i++) {
-	if (i == 0) { input = "\"" + input; }
 
+	// Insert quotes at start of key and end of value, and add a comma
 	if (input[i] == '\n') {
 		input = Insert(input,i + 1, "\"");
-		input = Insert(input,i, "\"");
+		input = Insert(input,i, "\",");
+
+		// Move i to compensate for adding 2 more string elements
 		i++;
 		i++;
 	}
 
-	else if (input[i] == ':') {
+	// Insert quote at end of key and start of value
+	// Also checks to make sure we're not at the end of the string
+	// otherwise next check will throw an outofbounds error
+	else if (input[i] == ':' && i != input.length + 1) {
+		// Makes sure next value after the colon is a space to stop quotation marks
+		// being inserted into urls or similar elements
+		if (input[i+1] == ' '){
 		input = Insert(input,i + 2, "\"");
 		input = Insert(input,i, "\"");
+
+		// Move i to compensate for adding 2 more string elements
 		i++;
 		i++;
+		}
 	}
 
-	console.log(i);
 }
-return input;
+return "{\"" + input + "\"}";
 }
 
-function Insert(input:string,position:number,value:string):string{
-
-	return input.slice(0,position) + value + input.slice(position,input.length);
+// Returns a string containing the given value inserted at the given index of the input string 
+function Insert(input:string,index:number,value:string):string{
+	return input.slice(0,index) + value + input.slice(index,input.length);
 }
